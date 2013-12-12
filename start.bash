9abe3e87ddbbd9934c73ca260ed36ed98229ee08
Ox To A Cart <ox.to.a.cart@gmail.com>
2013-12-12 08:19:06 -0600

    #1253 Using UDT for fallbacks

--- start.bash
+++ start.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup ./run.bash --disable-ui --oauth2-client-secrets-file=src/main/resources/client_secrets_installed.json --oauth2-user-credentials-file=/home/lantern/lantern_credentials.json $* &
+nohup ./quickRun.bash --disable-ui --oauth2-client-secrets-file=src/main/resources/client_secrets_installed.json --oauth2-user-credentials-file=/home/lantern/lantern_credentials.json $* &
